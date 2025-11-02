72cce10477606482cdc22c3e416ff47baa43b0d5
Ali Hameed Al-Hashime <ALi.hameed.it@gmail.com>
2025-11-02 14:35:25 +0300

    20251102 add backup part 2

--- database/seeders/PermissionSeederTemp.php
+++ database/seeders/PermissionSeederTemp.php
@@ -11,22 +11,22 @@ class PermissionSeederTemp extends Seeder
     public function run(): void
     {
         //region System Permissions
-        // $permissions = [
-        //      // Backup Management
-        //     ['name' => 'show backups', 'name_ar' => 'عرض النسخ الاحتياطية'],
-        //     ['name' => 'create backup', 'name_ar' => 'إنشاء نسخة احتياطية'],
-        //     ['name' => 'delete backup', 'name_ar' => 'حذف نسخة احتياطية'],
-        //     ['name' => 'restore backup', 'name_ar' => 'استعادة نسخة احتياطية'],
-        //     ['name' => 'download backup', 'name_ar' => 'تحميل نسخة احتياطية'],
-        //     ['name' => 'manage backup settings', 'name_ar' => 'إدارة إعدادات النسخ الاحتياطية'],
-        //     ['name' => 'manage backup admins', 'name_ar' => 'إدارة مسؤولي النسخ الاحتياطية'],
-        //     ['name' => 'show backup logs', 'name_ar' => 'عرض سجلات النسخ الاحتياطية'],
-        //     ['name' => 'show backup health', 'name_ar' => 'عرض حالة النسخ الاحتياطية'],
-        // ];
+        $permissions = [
+             // Backup Management
+            ['name' => 'show backups', 'name_ar' => 'عرض النسخ الاحتياطية'],
+            ['name' => 'create backup', 'name_ar' => 'إنشاء نسخة احتياطية'],
+            ['name' => 'delete backup', 'name_ar' => 'حذف نسخة احتياطية'],
+            ['name' => 'restore backup', 'name_ar' => 'استعادة نسخة احتياطية'],
+            ['name' => 'download backup', 'name_ar' => 'تحميل نسخة احتياطية'],
+            ['name' => 'manage backup settings', 'name_ar' => 'إدارة إعدادات النسخ الاحتياطية'],
+            ['name' => 'manage backup admins', 'name_ar' => 'إدارة مسؤولي النسخ الاحتياطية'],
+            ['name' => 'show backup logs', 'name_ar' => 'عرض سجلات النسخ الاحتياطية'],
+            ['name' => 'show backup health', 'name_ar' => 'عرض حالة النسخ الاحتياطية'],
+        ];
 
-        // foreach ($permissions as $permission) {
-        //     Permission::create($permission);
-        // }
+        foreach ($permissions as $permission) {
+            Permission::create($permission);
+        }
 
         $permissions = Permission::all();
         $adminRole = Role::where('name', 'Administrator')->first();
